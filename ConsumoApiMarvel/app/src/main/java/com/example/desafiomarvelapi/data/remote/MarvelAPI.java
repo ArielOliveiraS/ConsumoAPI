64fd614bd386d2cbbe669ed706ebe903a82588c7
Ariel <gildaariel@outlook.com>
2020-04-01 00:43:46 -0300

    adicionando endpoint comics

--- ConsumoApiMarvel/app/src/main/java/com/example/desafiomarvelapi/data/remote/MarvelAPI.java
+++ ConsumoApiMarvel/app/src/main/java/com/example/desafiomarvelapi/data/remote/MarvelAPI.java
@@ -2,6 +2,7 @@ package com.example.desafiomarvelapi.data.remote;
 
 import com.example.desafiomarvelapi.model.characters.Personagens;
 import com.example.desafiomarvelapi.model.charactersId.Characterid;
+import com.example.desafiomarvelapi.model.comics.ComicsResult;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
@@ -25,4 +26,14 @@ public interface MarvelAPI {
             @Query("hash") String hash,
             @Query("apikey") String apikey
     );
+
+    @GET("comics?")
+    Observable<ComicsResult> getAllComics(@Query("offset")int pagina,
+                                          @Query("format")String format,
+                                          @Query("formatType")String formatType,
+                                          @Query("noVariants")boolean noVariants,
+                                          @Query("orderBy") String orderBy,
+                                          @Query("ts") String ts,
+                                          @Query("hash") String hash,
+                                          @Query("apikey") String apikey);
 }
--- ConsumoApiMarvel/app/src/main/java/com/example/desafiomarvelapi/repository/MarvelRepository.java
+++ ConsumoApiMarvel/app/src/main/java/com/example/desafiomarvelapi/repository/MarvelRepository.java
@@ -1,15 +1,25 @@
 package com.example.desafiomarvelapi.repository;
 
-import com.example.desafiomarvelapi.data.remote.RetrofitService;
 import com.example.desafiomarvelapi.model.characters.Personagens;
+import com.example.desafiomarvelapi.model.charactersId.Characterid;
+import com.example.desafiomarvelapi.model.comics.ComicsResult;
 
 import io.reactivex.Observable;
 
+import static com.example.desafiomarvelapi.data.remote.RetrofitService.getApiService;
+
 public class MarvelRepository {
 
-    public Observable<Personagens> getPersonagemRepositori(int pagina, String orderBy, String ts,
-                                                           String hash, String apiKey) {
-        return RetrofitService.getApiService().getALLPersonagens(pagina, orderBy, ts, hash, apiKey);
+    public Observable<Personagens> getPersonagemRepositori(int pagina, String orderBy, String ts, String hash, String apiKey) {
+        return getApiService().getALLPersonagens(pagina, orderBy, ts, hash, apiKey);
+    }
+
+    public Observable<Characterid> getCharacterRepository(String ts, String hash, String apiKey) {
+        return getApiService().getIdCharacter(ts, hash, apiKey);
+    }
+
+    public Observable<ComicsResult> getComicsRepository(int pagina, String format, String formatType, boolean noVariants, String orderBy, String ts, String hash, String apiKey){
+        return getApiService().getAllComics(pagina, format, formatType, noVariants, orderBy, ts, hash, apiKey);
     }
 }
 
